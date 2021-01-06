4e6b7423d5298e2c7c68ef8a40ca1f49c0c9802c
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-01-06 09:28:03 +0100

    Add base files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Dream</title>
+</head>
+<body>
+
+<form action="action.php" method="post">
+<label for="amount">Amount:</label><br>
+<input type="number" id="amount" name="amount" value="1" min="0" ><br>
+ <p><input type="submit" /></p>
+</form>
+
+</body>
+</html>
