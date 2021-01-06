c9e36e675e6fe0e4365514c4b618fa76db01844e
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-01-06 13:29:02 +0100

    Add basic styling

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="./styles.css">
     <title>The Dream: Currency Converter</title>
 </head>
 <body>
@@ -13,37 +14,42 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 ?>
 
-<h1> </h1>
-
 <form method="post" name="form">
-<label for="amount">Amount:</label><br>
-<input type="number" id="amount" name="amount" placeholder="1" min="0"/>
-<br>
-<br>
 
-<select name="currency1" id="currency1">
-    <option value="taiwandollar">TWD 🇹🇼</option>
-    <option value="bath">Thai Bath 🇹🇭</option>
-</select>
-<br>
-<br>
+    <h1> Convert Your Money to Euros 💸 </h1>
 
-<select name="currency2" id="currency2">
-    <option value="euro">Euro 🇪🇺</option>
-</select>
-<br>
-<br>
+    <label for="amount">Amount:</label><br>
+    <input type="number" id="amount" name="amount" placeholder="1" min="0"/>
+    <br>
+    <br>
 
-<input name="submit" type="submit" />
+    <label for="currency1">From:</label><br>
+    <select name="currency1" id="currency1">
+        <option value="bath">Thai Bath 🇹🇭</option>
+        <option value="taiwandollar"> New Taiwan Dollar 🇹🇼</option>
+        <option value="japaneseyen"> Japanese Yen 🇯🇵</option>
+    </select>
 
-</form>
+    <br>
+    <br>
 
-<br>
+    <label for="currency2">To:</label><br>
+    <select name="currency2" id="currency2">
+        <option value="euro">Euro 🇪🇺</option>
+    </select>
+
+    <br>
+    <br>
+
+    <input name="submit" type="submit" value="Calculate" />
+
+    <br>
+    <br>
 
 <?php
 $taiwandollartoeuro = 0.0289540;
-$eurototaiwandollar = 34.5376;
 $thaibathtoeuro = 0.0271211;
+$japaneseyentoeuro = 0.00788118;
     
     if (isset($_POST["submit"])) { 
 
@@ -52,22 +58,27 @@ $thaibathtoeuro = 0.0271211;
 
         switch ($currency) {
             case 'taiwandollar':
-            $toeuros = $amount * $taiwandollartoeuro;
-            echo $amount . " in TWD is " . $toeuros . "€.";
+            $toeuros = round(($amount * $taiwandollartoeuro), 2);
+            echo $amount . " in TWD will get you " . $toeuros . " euros.";
             break;
             
             case 'bath':
-            $toeuros = $amount * $thaibathtoeuro;
-            echo $amount . " in Thai Bath is " . $toeuros . "€.";
+            $toeuros = round(($amount * $thaibathtoeuro), 2);
+            echo $amount . " in Thai Bath will get you " . $toeuros . " euros.";
+            break;
+
+            case 'japaneseyen':
+            $toeuros = round(($amount * $japaneseyentoeuro), 2);
+            echo $amount . " in JPY will get you " . $toeuros . " euros.";
             break;
 
             default:
+            echo "Please submit your amount";
             break;
         }
     }
-
 ?> 
 
-
+</form>
 </body>
 </html>
